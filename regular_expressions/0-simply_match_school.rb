fef36931554170fa48a663ed339818d8066acdb0
Immaculata <e.emmanuel@alustudent.com>
2025-10-26 08:20:21 -0700

    Updated 0-simply match school

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,8 +1,2 @@
 #!/usr/bin/env ruby
-# This script prints "School" if the argument contains the word "School"
-
-if ARGV[0]
-  match = ARGV[0].match(/School/)
-  puts match[0] if match
-end
-
+puts ARGV[0].scan(/School/).join
